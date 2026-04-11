56920ec6bd7bddc22f25e67addd415334a4e4848
lukasloetkolben <61192133+lukasloetkolben@users.noreply.github.com>
2026-04-11 11:43:53 +0200

    xnor-tech: F4 panda support, Tesla/VW MEB ignition detection
    - Add F4 device types (WHITE, BLACK) to supported devices
    - Add VW MEB ignition detection via CAN address 0x3C0
    - Add Tesla Model S legacy ignition detection via GTW_status (0x348)
    - Add deprecated HW warning for F4 pandas

--- board/drivers/can_common.h
+++ board/drivers/can_common.h
@@ -156,6 +156,8 @@ void can_set_forwarding(uint8_t from, uint8_t to) {
 #endif
 
 void ignition_can_hook(CANPacket_t *msg) {
+  int len = GET_LEN(msg);
+
   if (msg->bus == 0U) {
     int len = GET_LEN(msg);
 
@@ -200,7 +202,26 @@ void ignition_can_hook(CANPacket_t *msg) {
       ignition_can = (msg->data[0] >> 5) == 0x6U;
       ignition_can_cnt = 0U;
     }
+
+    // Volkswagen MEB exception
+    if ((msg->addr == 0x3C0U) && (len == 4)) {
+      ignition_can = GET_BIT(msg, 17U);
+      ignition_can_cnt = 0U;
+    }
   }
+
+  // Tesla Model S exception
+  if (((msg->bus == 0) || (msg->bus == 1)) && (msg->addr == 0x348U) && (len == 8)) {
+     int counter = msg->data[6] & 0xFU;
+
+     static int prev_counter_tesla_legacy = -1;
+     if ((counter == ((prev_counter_tesla_legacy + 1) % 16)) && (prev_counter_tesla_legacy != -1)) {
+       // GTW_status
+       ignition_can = (msg->data[0] & 0x1U) != 0U;
+       ignition_can_cnt = 0U;
+     }
+     prev_counter_tesla_legacy = counter;
+ }
 
   // body exception
   if (((msg->bus == 0U) || (msg->bus == 2U)) && (msg->addr == 0x201U)) {
